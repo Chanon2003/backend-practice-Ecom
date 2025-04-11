444edd4fab854d6cf4a02ccd3804e83ced67f401
Chanon2003 <ch.chanonlienwork@gmail.com>
2025-04-11 15:44:02 +0700

    soi test4

--- server/route/images.route.js
+++ server/route/images.route.js
@@ -4,7 +4,7 @@ import { deleteImages, updateImages, uploadImages } from "../controller/uploadIm
 
 const images = Router();
 
-//route ทดลอง image
+//route ทดลอง images
 images.post('/upload-images',upload.array('images'),uploadImages);
 images.post('/update-images',upload.array('images'),updateImages);
 images.delete('/delete-images',upload.array('images'),deleteImages);
